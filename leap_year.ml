f2117ee101dc07ce52e747ef6f649d7689ce4d93
kemsguy7 <mattidungafa@gmail.com>
2025-05-15 12:02:33 +0100

    changed from standard input to system arguments

--- leap_year.ml
+++ leap_year.ml
@@ -1,7 +1,10 @@
-let year = read_int ()
+(*let year = read_int  *)
+
+(* rather than reading the  year from the standard input, we can pass it on the command line *)
+let year = int_of_string Sys.argv.(1)
 let leap =
   (year mod 4 = 0 && year mod 100 <> 0) || year mod 400 = 0 
 let msg = if leap then "is" else "is not" 
-let () = Printf.printf "%d %s a leap year\n" year msg   
+let () = Printf.printf "%d %s a leap year\n" year msg    (* used to maintain the smae formate in case of expression that do not return a value *)
 
 
