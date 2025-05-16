cb728aaefa39e90176521e9f09c1f66b3746ffbc
kemsguy7 <mattidungafa@gmail.com>
2025-05-16 14:20:27 +0100

    added more explanations to the functions

--- approxpi.ml
+++ approxpi.ml
@@ -1,12 +1,12 @@
 let n = read_int() 
 
 let () = 
-  let p = ref 0 in 
+  let p = ref 0 in (* p  is a variable that takes a referecne of 0 *)
   
-  for k = 1 to n do 
+  for k = 1 to n do (* start of thr for loop *)
     let x =  Random.float 1.0 in 
     let y =  Random.float 1.0 in 
-    if x *. x +. y *. y <= 1.0 then 
+    if x *. x +. y *. y <= 1.0 then (*increases the contents of the reference p if the point (is within the quartr circle, that is, if x2 + y2 ≤ 1 ) *)
       p := !p + 1 
   done; 
   let pi = 4.0 *. float !p /. float n in 
