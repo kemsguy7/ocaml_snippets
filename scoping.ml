fefd88414d4aaf63261e54599c5428517acb7722
kemsguy7 <mattidungafa@gmail.com>
2025-05-18 07:31:12 +0100

    Added for reverse for loop

--- scoping.ml
+++ scoping.ml
@@ -9,3 +9,11 @@ let () =
   for i = (Printf.printf "*"; 0) to (Printf.printf "."; 5) do 
     Printf.printf "%d" i 
   done
+
+(* If the value of e1 is greater than e2 ,the body of the loop never gets executed *)
+
+(* variant of a for construct that goes backwards*)
+let () = 
+  for  i = 9 downto 0 do 
+    Printf.printf "%d\n" i 
+  done  
