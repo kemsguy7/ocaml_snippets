755b9839ba4d169154ef45bbd8ae12c5002fa643
kemsguy7 <mattidungafa@gmail.com>
2025-05-18 07:21:28 +0100

    Added for lopp to scoping file

--- scoping.ml
+++ scoping.ml
@@ -2,3 +2,10 @@ let x = 10
 let y = x + 2
 let x = 20 
 let () = Printf.printf "x=%d y=%d\n" x y
+
+
+(* Adding For loop evaluation code *)
+let () = 
+  for i = (Printf.printf "*"; 0) to (Printf.printf "."; 5) do 
+    Printf.printf "%d" i 
+  done
