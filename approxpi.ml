50f72c0dfff8bf75788a20b3a20812f9fe3c337f
kemsguy7 <mattidungafa@gmail.com>
2025-05-18 06:51:54 +0100

    updates : added comment to let code block

--- approxpi.ml
+++ approxpi.ml
@@ -9,5 +9,6 @@ let () =
     if x *. x +. y *. y <= 1.0 then (*increases the contents of the reference p if the point (is within the quartr circle, that is, if x2 + y2 ≤ 1 ) *)
       p := !p + 1 
   done; 
-  let pi = 4.0 *. float !p /. float n in 
-  Printf.printf "%f\n" pi 
+  let pi = 4.0 *. float !p /. float n in  (* the float function  defined in Stdlib module is used to convert integers into floats *)
+  Printf.printf "%f\n" pi  
+
