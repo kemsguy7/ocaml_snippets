3d6f4ed76b5a243ad85efe8522f46e080470c242
kemsguy7 <mattidungafa@gmail.com>
2025-05-21 16:52:03 +0100

    updates: introducing float expontiation

--- real_ocaml/overview.ml
+++ real_ocaml/overview.ml
@@ -7,4 +7,8 @@ let another_tuple=(3,"four",5.);;
 (** you can extract the components of a tuple using ocaml's pattern-matching syntax as shown below:  *)
 
 let (x, y) = a_tuple in
-x + String.length y;;
+x + String.length y;; 
+
+(* Function for compmuting the distance between 2 points rr *)
+let distance (x1,y1) (x2, y2) = 
+  Float.sqrt ((x1 -. x2) ** 2. +. (y1 -. y2) ** 2.);; 
