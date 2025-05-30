c3e26a2b9a35b739d023fc1fc80d9e20017b9592
kemsguy7 <mattidungafa@gmail.com>
2025-05-30 05:18:57 +0100

    updates : started mandelbrot script

--- mandelbrot.ml
+++ mandelbrot.ml
@@ -17,3 +17,19 @@ let mandelbrot a b =
       mandel_rec x' y' (i + 1)
   in 
   mandel_rec 0. 0. 0 
+
+let draw () = 
+  for w = 0 to width - 1 do 
+    for h = 0 to height - 1 do 
+      let a = 4. *. float w /. float width -. 2. in 
+      let b = 4. *. float h /. float height -. 2. in 
+      if mandelbrot a b then plot w h 
+    done 
+  done 
+
+let () = 
+  let dim = Printf.sprintf "%dx%d" width height in 
+  open_graph dim; 
+  draw (); 
+  draw(); 
+  ignore (read_key ()) 
