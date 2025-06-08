3ba903a3fb9cd54bbef918709d1a567c24254de6
kemsguy7 <mattidungafa@gmail.com>
2025-06-08 01:45:00 +0100

    udpates :started  anonymous function and partial application

--- functions.ml
+++ functions.ml
@@ -6,3 +6,18 @@ square (inc 5);;
 5 |> inc |> square ;;  (*uses the pipeline operato to 5 through the inc function, then send the result of that through the square function *)
 
 
+(*optional arguments*)
+let f ?name:(arg1=8) arg2 = arg1 + arg2;;
+
+(* This function takes an optional argument with a default value of 8, and a required argument. 
+   If the optional argument is not provided, it defaults to 8. *)
+
+
+(* Partial Application
+
+* The following functions are syntatically different but semantically equivalent.
+*)
+
+let add x y = x + y;; 
+let add x = fun y -> x + y;; 
+let add = fun x -> (fun y -> x + y);; 
