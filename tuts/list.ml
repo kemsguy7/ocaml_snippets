10499fc28b84cd4b726aad9a350afed711af402a
kemsguy7 <mattidungafa@gmail.com>
2025-06-11 18:34:46 +0100

    update: function to determine an empty list

--- tuts/list.ml
+++ tuts/list.ml
@@ -16,4 +16,16 @@ let rec sum xs =
     match lst1 with 
     | [] -> lst2  
      | h::t -> h:: append t lst2;;
-(* append function takes two lists and appends the first list to the second list *)
+(* append function takes two lists and appends the first list to the second list *) 
+
+
+(* function to determine if a list is empty*) 
+
+let empty lst = 
+  match lst with 
+  | [] -> true 
+  | h :: t -> false 
+
+  (*writing same function without pattern matching *)
+  let empty list  = 
+    lst = []
