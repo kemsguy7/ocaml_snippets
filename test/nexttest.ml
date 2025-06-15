0aeb17f47f4c7807457f2bdec565e32a1b3c4750
kemsguy7 <mattidungafa@gmail.com>
2025-06-15 02:49:59 +0100

    updates : refactored test by abstracting a function that creates test cases

--- test/nexttest.ml
+++ test/nexttest.ml
@@ -1,11 +1,22 @@
 open OUnit2 
 open Weekday 
-
+(* 
 let tests = "test suite for next_weekday" >::: [ 
     "tue_after_mon" >:: (fun _ -> assert_equal Tuesday (next_weekday Monday));
     "wed_after_tue" >:: (fun _ -> assert_equal Wednesday (next_weekday Tuesday));
     "thu_after_wed" >:: (fun _ -> assert_equal Thursday (next_weekday Wednesday));
     "fri_after_thu" >:: (fun _ -> assert_equal Friday (next_weekday Thursday));
- ] 
+ ]  *)
 
- let _ = run_test_tt_main tests 
+
+
+ (* Abstracting a function that creates test cases for next_weekday to avoid repetion*)
+ 
+let make_next_weekday_test name expected_output input = 
+    name >:: (fun _ ->  assert_equal expected_output (next_weekday input))
+
+let tests = "test suite for next_weekday" >::: [ 
+    make_next_weekday_test "tue_after_mon" Tuesday Monday;
+]
+
+let _ = run_test_tt_main tests 
