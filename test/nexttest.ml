00a0c23b1c2e1654fd6c87293e655ec2c65afbc9
kemsguy7 <mattidungafa@gmail.com>
2025-06-15 02:31:17 +0100

    updates : added more test cases

--- test/nexttest.ml
+++ test/nexttest.ml
@@ -3,6 +3,9 @@ open Weekday
 
 let tests = "test suite for next_weekday" >::: [ 
     "tue_after_mon" >:: (fun _ -> assert_equal Tuesday (next_weekday Monday));
+    "wed_after_tue" >:: (fun _ -> assert_equal Wednesday (next_weekday Tuesday));
+    "thu_after_wed" >:: (fun _ -> assert_equal Thursday (next_weekday Wednesday));
+    "fri_after_thu" >:: (fun _ -> assert_equal Friday (next_weekday Thursday));
  ] 
 
- let _ = run_test_tt_main tests
+ let _ = run_test_tt_main tests 
