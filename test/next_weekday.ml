fbe949018aa931f668ffad86236d7768f501f8b1
kemsguy7 <mattidungafa@gmail.com>
2025-06-15 01:57:01 +0100

    refactoring the sum test and started writing test for next_Weekday

---
+++ test/next_weekday.ml
@@ -0,0 +1,8 @@
+(*This script shows different levels of tdd*)
+
+type day = Sunday | Monday | Tuesday | Wednesday | Thursday | Friday | Saturday 
+
+(* most basic , broken version of the function *)
+ let next_weekday d = failwith "Unimplemented"  
+
+ (*write the simplest unit test  *)
--- test/test.ml
+++ test/test.ml
@@ -1,10 +1,22 @@
 open OUnit2 
 open Sum 
 
-let tests = "test suit for sum" >::: [ 
+(* test cases for teh sum function *)
+(* let tests = "test suit for sum" >::: [ 
   "empty" >:: (fun _ -> assert_equal 0 (sum []));
   "singleton" >:: (fun _ -> assert_equal 1 (sum [1]));
   "two_elements" >:: (fun _ -> assert_equal 3 (sum [1; 2]));
+] *)
+
+
+
+(*rewriting test suite to print better error messages *)
+let tests = "test suit for sum" >::: [ 
+  "empty" >:: (fun _ -> assert_equal 0 (sum []) ~printer:string_of_int);
+  "singleton" >:: (fun _ -> assert_equal 1 (sum [1]) ~printer:string_of_int);
+  (* using ~printer to print the expected and actual values in case of failure *)
+  "two_elements" >:: (fun _ -> assert_equal 3 (sum [1; 2]) ~printer:string_of_int);
 ]
+
 
 let _ = run_test_tt_main tests  
