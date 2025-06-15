6d853e1d480ab02eb2ffbba70bede9d72d7cc8e4
kemsguy7 <mattidungafa@gmail.com>
2025-06-15 02:32:08 +0100

    updates : added more test cases

--- test/weekday.ml
+++ test/weekday.ml
@@ -9,5 +9,9 @@ type day = Sunday | Monday | Tuesday | Wednesday | Thursday | Friday | Saturday
  let next_weekday d = 
    match d with 
    | Monday -> Tuesday 
+   | Tuesday -> Wednesday
+   | Wednesday -> Thursday
+   | Thursday -> Friday
+   | Friday -> Saturday
    | _ -> failwith "Unimplemented"
  
