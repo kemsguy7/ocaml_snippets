9906cdfa697544c8b699c170930a222d8997099d
kemsguy7 <mattidungafa@gmail.com>
2025-06-15 03:07:43 +0100

    updates: completed tests cases

--- test/nexttest.ml
+++ test/nexttest.ml
@@ -17,6 +17,23 @@ let make_next_weekday_test name expected_output input =
 
 let tests = "test suite for next_weekday" >::: [ 
     make_next_weekday_test "tue_after_mon" Tuesday Monday;
+    make_next_weekday_test "wed_after_tue" Wednesday Tuesday;
+    make_next_weekday_test "thu_after_wed" Thursday Wednesday;
+    make_next_weekday_test "fri_after_thu" Friday Thursday;
+    make_next_weekday_test "sat_after_fri" Saturday Friday;
+    make_next_weekday_test "sun_after_sat" Sunday Saturday;
+
+    (* handling weekends *)
+
+   make_next_weekday_test "mon_fter_fri" Monday Friday; 
+   make_next_weekday_test "mon_after_sat" Monday Saturday; 
+   make_next_weekday_test "mon_after_sun" Monday Sunday;
+
+    (* Adding a test for an unimplemented case *)
+    (* This will fail until we implement the case in the next_weekday function *)
+
+    (* Adding a test for the unimplemented case *)
+    "unimplemented_case" >:: (fun _ -> assert_raises (Failure "Unimplemented") (fun () -> next_weekday Sunday));
 ]
 
 let _ = run_test_tt_main tests 
--- test/weekday.ml
+++ test/weekday.ml
@@ -12,6 +12,9 @@ type day = Sunday | Monday | Tuesday | Wednesday | Thursday | Friday | Saturday
    | Tuesday -> Wednesday
    | Wednesday -> Thursday
    | Thursday -> Friday
-   | Friday -> Saturday
-   | _ -> failwith "Unimplemented"
+   | Friday -> Monday
+   | Saturday -> Monday
+   | Sunday -> Monday
+   (* Adding a case for Sunday to return Monday, as it is the next weekday *)
+   (* This will make the test pass, but we can add more tests for unimplemented cases later *)
  
