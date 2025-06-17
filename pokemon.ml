ad398b0246639fe67184344ce4a818b2d5582d04
kemsguy7 <mattidungafa@gmail.com>
2025-06-17 14:23:05 +0100

    modified the script to fix build errors

--- pokemon.ml
+++ pokemon.ml
@@ -1,24 +1,25 @@
 (* A script to model a pokemon game *)
 
-(* Building  the types *)
+(* Building the types *)
 type ptype = TNormal | TFire | TWater 
 
 (* Effectiveness of attack *)
-
-type peff = ENormal |ENotVery | ESuper  
+type peff = ENormal | ENotVery | ESuper  
  
 (* mult_of_eff *)
-let mult_of_eff =  function 
+let mult_of_eff = function 
   | ENormal -> 1. 
   | ENotVery -> 0.5 
   | ESuper -> 2.0
 
 let eff = function 
-  (* | (TFire, TFire) -> ENotVery  *)
-  (* rewriting the pattern above with or syntax *)
-  |(TFire, TFire) | (TWater, TWater) | (TFire, TWater)-> ENotVery 
+  | (TFire, TFire) | (TWater, TWater) | (TFire, TWater) -> ENotVery 
   | (TWater, TFire) -> ESuper 
-    | _ ->ENormal
+  | _ -> ENormal
   
-  let () =
-    Printf.printf "%f\n" (mult_of_eff (eff (TFire, TFire)))
+let () =
+  (* Using all constructors to silence warnings *)
+  let _ = TNormal in  (* This line just shows we're aware of TNormal *)
+  Printf.printf "Fire vs Fire: %f\n" (mult_of_eff (eff (TFire, TFire)));
+  Printf.printf "Water vs Fire: %f\n" (mult_of_eff (eff (TWater, TFire)));
+  Printf.printf "Normal vs Water: %f\n" (mult_of_eff (eff (TNormal, TWater)))
