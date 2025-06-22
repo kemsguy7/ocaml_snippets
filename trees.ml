a01668b11d515a30075cac5d1c659cdf7df9e736
kemsguy7 <mattidungafa@gmail.com>
2025-06-22 15:26:00 +0100

    started tree with records

--- trees.ml
+++ trees.ml
@@ -11,7 +11,7 @@
 
 *)
 
-type 'a tree = 
+type 'a tree =  (* Alpha type shows that this type can be a string , int or float *)
   | Leaf 
   | Node of 'a * 'a tree * 'a tree 
 
@@ -28,4 +28,25 @@ let t = (
     )
   )
 )
+
+
+(* Rewriting binary tree with Records *)
+type 'a tree = 
+  | Leaf  (* Lead will always be empty *)
+  | Node of 'a node 
+
+and 'a node = { 
+  value : 'a;
+  left: 'a tree; 
+  right : 'a tree
+}
+
+let rt = Node { 
+  value = 2; 
+  left = Node {value =1; left = Leaf; right = Leaf};
+  right = Node {value=3 ; left = Leaf; right = Leaf}
+}
+(*Try adding in another level of nesting *)
+
+
 
