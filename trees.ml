0403dcf854f7d93935c4425953f75711a4f6cc70
kemsguy7 <mattidungafa@gmail.com>
2025-06-22 16:17:51 +0100

    updates : traversing the tree  with pattern matching

--- trees.ml
+++ trees.ml
@@ -41,12 +41,48 @@ and 'a node = {
   right : 'a tree
 }
 
+(*
+          2 
+        /   \
+      1      3 
+*)
+
 let rt = Node { 
   value = 2; 
-  left = Node {value =1; left = Leaf; right = Leaf};
+  left =Node {value =1; left = Leaf; right = Leaf};
   right = Node {value=3 ; left = Leaf; right = Leaf}
 }
+
 (*Try adding in another level of nesting *)
 
+  (*
+           2
+          /  \
+        1      3           
+      /  \        
+      5   7    
+
+*)
+
+let rt = 
+  Node { 
+    value = 2; 
+    left = Node {  (* Left subtree: Node 1 with its own left/right *)
+      value = 1; 
+      left = Node { value = 5; left = Leaf; right = Leaf };  (* Nested left *)
+      right = Node {value = 7; left= Leaf; right = Leaf }; 
+    }; 
+    right = Node { 
+      value = 3; 
+      left = Leaf; 
+      right = Leaf 
+    } 
+  }
 
 
+  (*traversing the tress with pattren matching *)
+(** [mem x t] is whether [x] is a value at some node in tree [t]. **)
+
+let rec mem x = function 
+  | Leaf -> false 
+  | Node {value; left; right} -> value = x || mem x left || mem x right
