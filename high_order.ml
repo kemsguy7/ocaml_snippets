7adb888d88fd6cd5789a8ba8123549008862ca9d
kemsguy7 <mattidungafa@gmail.com>
2025-06-25 15:31:47 +0100

    adding custom logic for filtering even and odd numbers

--- high_order.ml
+++ high_order.ml
@@ -18,4 +18,25 @@ concat_bang [2;4;5]
 
 
 (* Filter *)
-(***)
+(** [even n] is whether [n] is even. *)
+let even n = 
+  n mod 2 = 0 
+
+(** [even lst] is the sublist of [lst] containing only even numbers. *)
+let rec evens = function 
+  | [] -> []
+  | h :: t -> if even h then h :: evens t else evens t 
+
+let lst1  = evens [ 1; 2; 3; 4] 
+
+
+
+(** [odds lst] is the sublist of [lst] containing only odd numbers. *)
+let rec odds = function 
+  | [] -> []
+  | h ::t -> if odd h then h :: odds t else odd t 
+
+let lst2 = odds [1; 2; 3; 4]
+
+
+
