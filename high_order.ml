3d97e8fa83c153f8da7dbe444de323ba0e106cc6
kemsguy7 <mattidungafa@gmail.com>
2025-06-25 15:48:42 +0100

    adding custom filter function

--- high_order.ml
+++ high_order.ml
@@ -30,13 +30,20 @@ let rec evens = function
 let lst1  = evens [ 1; 2; 3; 4] 
 
 
+(** [odd n] is whether [n] is odd *)
+let odd n = 
+  n mod 2 <> 0 
+
 
 (** [odds lst] is the sublist of [lst] containing only odd numbers. *)
 let rec odds = function 
   | [] -> []
-  | h ::t -> if odd h then h :: odds t else odd t 
+  | h ::t -> if odd h then h :: odds t else odds t 
 
 let lst2 = odds [1; 2; 3; 4]
 
 
 
+let rec filter p = function 
+  | [] -> []
+  | h :: t -> if p h then h :: filter p t else filter p t   
