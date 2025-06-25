3acc50c891e48998a80ca8bfa90362bf76255342
kemsguy7 <mattidungafa@gmail.com>
2025-06-25 13:56:28 +0100

    updates : started working with high order functions[filter]

--- high_order.ml
+++ high_order.ml
@@ -9,10 +9,13 @@ let rec map f = function
 
 (* [add1 lst] adds 1 to each element of [slt]. *)
 let add1 = map (fun x -> x + 1)
+add1 [4;5;5]
 
 
 (** [concat_bang lst] concatenates "!" to each element of [lst]. *)
 let concat_bang = map (fun x -> x ^ "!")
+concat_bang [2;4;5]
 
 
-
+(* Filter *)
+(***)
