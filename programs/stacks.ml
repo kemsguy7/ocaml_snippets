a562337793578d5f6629214af81b7327ce64c757
kemsguy7 <mattidungafa@gmail.com>
2025-06-28 07:26:42 +0100

    added type definitions to the ListStack module

--- programs/stacks.ml
+++ programs/stacks.ml
@@ -1,3 +1,26 @@
+module type LIST_STACK = sig 
+  
+  (** [EMPTY] is raised when an operation cannot be applied to an empty stack *)
+  exception Empty 
+
+  (** [empty] is the empty stack. *)
+  val empty : 'a list  
+
+  (** [is_empty s] is whether [s] is empty *)
+  val is_empty  : 'a list -> bool 
+
+  (** [push x s] pushes [x] onto the top of [s]. *)
+  val push : 'a-> 'a list -> 'a list 
+
+  (** [peek s] is the top element of [s]. Raises [Empty] is [s] is empty. *)
+  val peek : 'a list -> 'a 
+
+  (** [pop s] is all but the top Element of [s]. 
+    Raises [Empty] if [s] is empty. *)  
+  val pop : 'a list -> 'a list 
+end
+
+
 module ListStack  = struct 
   type 'a stack = 'a list 
   
