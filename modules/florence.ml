b5b9b0d8bed37987575d4b66f398fc1850ecb451
kemsguy7 <mattidungafa@gmail.com>
2025-07-03 14:50:42 +0100

    updates: working with modules and signatures

--- modules/florence.ml
+++ modules/florence.ml
@@ -5,3 +5,16 @@ end
 
 let print_goodbye () = print_endline "Goodbye" 
 
+
+(* Submodules with signatures *)
+
+(*.To determine a submodule's interfac, we can provide a module signature *)
+module Hello : sig 
+  val print: unit -> unit 
+end = struct 
+  let message = "Hello"
+  let print () = print_endline message 
+end 
+
+
+let print_goodbye () = print_endline "Goodbye"
