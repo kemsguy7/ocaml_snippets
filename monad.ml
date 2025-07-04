02791b1b4e8f25dc6c88bfa124c4f2f5ca6e75e4
kemsguy7 <mattidungafa@gmail.com>
2025-07-04 22:33:50 +0100

    added structure of the maybe monad

--- monad.ml
+++ monad.ml
@@ -7,13 +7,30 @@ Value has type 'a
 - bind take as input : 
    - a boxed value, which has type 'a t, and 
    - A function that itself takes an unboxed value of type 'a as input and returns a boxed value of type 'b t as output
+- Bind applies it's second argument to the first. That requires taking the 'a value out of it's box, applying the function to it , and returning the result. 
 
 *)
 
 module type Monad = sig 
   type 'a t  
   val return : 'a -> 'a t 
-  val bind : 'a t -> ('a -> 'b t) -> 'b t
+  (* val bind : 'a t -> ('a -> 'b t) -> 'b t *)
+
+  val ( >>= ) : 'a t -> ('a -> 'b t) -> 'b t
 end 
 
+
+(* Implementation of the monad signature for the maybe monad *)
+
+module Maybe : Monad = struct 
+  type 'a t = 'a option 
+  
+  let return x = Some x 
+
+  let (>>=) m f = 
+    match m with 
+    | None -> None 
+    | Some x -> f x 
+end 
+    
 
