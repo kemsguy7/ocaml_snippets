21c1a0517b2f723d3bf37653683eca48e24f6296
kemsguy7 <mattidungafa@gmail.com>
2025-07-09 04:31:08 +0100

    briging a module into scorpe of a function

--- modules/scope.ml
+++ modules/scope.ml
@@ -11,3 +11,11 @@ exampels below
 let s = "BigRed "
 let s' = s |> String.trim |> String.lowercase_ascii (* long way *)
 let s'' = String.(s |> trim |> lowercase_ascii) (* short way *) 
+
+
+(* To bring the above module into scope for an entire function or large code block, you can use the expression. let open M in e  *)
+
+(** [lower_trim s] is [s] in lower case with whitespace removed  *)
+let lower_trim s = 
+  let open String in  
+  s |> trim |> lowercase_ascii
