d9843247439fbd7702e02102ad9b2f4f216f4122
kemsguy7 <mattidungafa@gmail.com>
2025-07-12 16:11:48 +0100

    updates: started working with refs

---
+++ ref.ml
@@ -0,0 +1,19 @@
+
+(* This function takes a value in memory and returns an addition to the previous value in memory when called *)
+
+(* let counter = ref 0 
+
+let next_val = 
+  fun () -> 
+    counter := !counter + 1 ;
+    !counter  *)
+
+(* code above can also be written as below to make the counter variable not exposed to the outside world *)
+
+
+let next_val = 
+  let counter = ref 0 in 
+    fun () -> 
+    incr counter; 
+     !counter 
+
